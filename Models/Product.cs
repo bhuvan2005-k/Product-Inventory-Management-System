5e0df159e4f21ecc376c115ab32cca79b50f755f
Bhuvaneshwari K <bhuvaneshwari1420@gmail.com>
2025-12-13 00:16:20 +0530

    Add Product model with properties

---
+++ Models/Product.cs
@@ -0,0 +1,11 @@
+namespace ProductInventoryAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
